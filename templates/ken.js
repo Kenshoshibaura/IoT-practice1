c6afa45907f5c46954b72411601868a44aa86c81
Kenshoshibaura <50857122+Kenshoshibaura@users.noreply.github.com>
2020-03-28 23:40:04 +0900

    ver9 サイズ調整完了

--- templates/ken.js
+++ templates/ken.js
@@ -1,6 +1,17 @@
 
 setInterval('showClock()',1000);
 
+/*
+window_load();
+window.onresize = window_load;
+function window_load(){
+  var sW,sH,s;
+  sW = window.innerWidth;
+  sH = window.innerHeight;
+  s = "ウィンドウサイズ:横幅 = " + sW + " / 高さ = " + sH;
+  document.getElementById('winsize').innerHTML = s;
+}*/
+
 function set2fig(num){
   var ret;
   if(num<10){ret="0"+num;}
